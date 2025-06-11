d2ccce2c265e3319030dd8c0b8b9addfa0302c4b
arca-lignea <33169190+arca-lignea@users.noreply.github.com>
2025-06-11 23:54:29 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
   // must end with newline or will get error "streams had content in them that was not flushed"
-  cout << "Hello, Emscripten!" << endl;
+  cout << "Hello, Emscripten sis!" << endl;
   printf("Hello, Emscripten bro!\n");
 
   // do not "return 0;"
