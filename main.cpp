73ea13656f22102d9ed79a3aefc9190593f30b7c
arca-lignea <33169190+arca-lignea@users.noreply.github.com>
2025-06-11 23:51:04 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
   // must end with newline or will get error "streams had content in them that was not flushed"
   cout << "Hello, Emscripten!" << endl;
-  printf("Hello, Emscripten!\n");
+  printf("Hello, Emscripten bro!\n");
 
   // do not "return 0;"
   // ref: https://emscripten.org/docs/getting_started/FAQ.html#what-does-exiting-the-runtime-mean-why-don-t-atexit-s-run
